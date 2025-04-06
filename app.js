8fd9985a76e561ae41a7f763ba827baff6a0b42d
Malerosky <malero_tin65@hotmail.com>
2025-04-06 19:47:28 -0400

    Termino del desafio 1

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-let numeroUsuario1
-let numeroUsuario2
-let resultado
+// Desafío 1: Hacer una suma de dos numeros mediante un prompt
+let numeroUsuario1 = parseFloat(prompt("Introduce un número"));
+console.log(numeroUsuario1);
 
+let numeroUsuario2 = parseFloat(prompt("Introduce un segundo número"));
+console.log(numeroUsuario2);
 
-numeroUsuario1 = alert(prompt("Introduce un número"));
-
-console.log("numeroUsuario1");
+let resultado = numeroUsuario1 + numeroUsuario2;
+alert("El resultado de la suma es: " + resultado);
+console.log(resultado);
